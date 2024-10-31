59665a8996625b6ea1f58bf386c25d8f155776aa
Mantosh Kumar <kumar.mantosh83@gmail.com>
2024-10-31 21:56:49 +0530

    Created initial project and UI

---
+++ CoreDataSample/CoreDataSample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CoreDataSample
+//
+//  Created by Mantosh Kumar on 31/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
