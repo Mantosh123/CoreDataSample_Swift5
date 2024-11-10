1f33c4f1bb4ffc0a59e063c6a25afee1ee76b42e
Mantosh Kumar <kumar.mantosh83@gmail.com>
2024-11-10 20:11:35 +0530

    Add update image functionality

--- CoreData_CRUD/CoreData_CRUD/EmployeeDetailViewController.swift
+++ CoreData_CRUD/CoreData_CRUD/EmployeeDetailViewController.swift
@@ -7,15 +7,15 @@
 
 import UIKit
 
-class EmployeeDetailViewController: UIViewController {
+class EmployeeDetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var profilePic: UIImageView!
     @IBOutlet weak var editButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
-    
+    let picker = UIImagePickerController()
+
     var selectedEmployee: Employee? = nil
-    
     var manager = EmployeeManager()
         
     override func viewDidLoad() {
@@ -25,14 +25,42 @@ class EmployeeDetailViewController: UIViewController {
         emailTextField.text = selectedEmployee?.emailId
     }
     
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let image = info[.originalImage] as? UIImage
+        profilePic.image = image
+        
+        dismiss(animated: true)
+        
+    }
+    
     @IBAction func updateButtonactipon(_ sender: Any) {
         
+        let employee = Employee(id: selectedEmployee!.id, name: nameTextField.text, emailId: emailTextField.text, profilePic: profilePic.image?.pngData())
+        let updatestatus = manager.updateEmployee(employee: employee)
         
+        if updatestatus {
+            navigationController?.popViewController(animated: true)
+        } else {
+            print("Something went wrong")
+        }
     }
     
     @IBAction func deleteButtonAction(_ sender: Any) {
         
+        if manager.deleteEmployee(employee: selectedEmployee!) {
+            navigationController?.popViewController(animated: true)
+        } else {
+            print("Something went wrong")
+        }
     }
     
+    @IBAction func profileImageEditAction(_ sender: Any) {
+        
+        picker.sourceType = .savedPhotosAlbum
+        picker.delegate = self
+        present(picker, animated: true)
+    }
     
 }
